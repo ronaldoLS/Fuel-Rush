62c2ea730e92eb35d6224ea6288524ac28a1a382
Ronaldo Santos <65672971+ronaldoLS@users.noreply.github.com>
2026-03-14 13:07:26 +0000

    implement start button scene loading

--- Assets/Scripts/UIMenuController.cs
+++ Assets/Scripts/UIMenuController.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMenuController : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startButton.onClick.AddListener(StartGame);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void StartGame()
+    {
+        // Load the main game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("My Game");
     }
 }
