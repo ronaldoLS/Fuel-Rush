810ce09cf97c2b30e2783b7eaf41394bb436f742
Ronaldo Santos <65672971+ronaldoLS@users.noreply.github.com>
2026-03-15 14:05:46 +0000

    Save/load high score and display record
    Persist player record between sessions: add SaveData class and JSON save/load methods in GameManager (SaveRecord, LoadRecord, CleanRecord) using Application.persistentDataPath/savefile.json. Introduce a record field, update it on GameOver when distance > record, and initialize/load the record on Awake/Start. Update UIMenuController to show the record in the main menu via a TextMeshProUGUI and call GameManager.LoadRecord; adjust DefaultExecutionOrder attributes and minor initialization changes. Also add recovery scene asset and corresponding .meta files.

--- Assets/Scripts/UIMenuController.cs
+++ Assets/Scripts/UIMenuController.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
+[DefaultExecutionOrder(10)]
 public class UIMenuController : MonoBehaviour
 {
     [SerializeField] private Button startButton;
+    [SerializeField] private TextMeshProUGUI recordText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
+        
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.LoadRecord();
+            if (GameManager.Instance.record > 0)
+                recordText.text = $"Record: {GameManager.Instance.record:F2} m";
+            else
+                recordText.text = "";
+
+        }else
+        {
+            recordText.text = "";
+            Debug.LogWarning("GameManager instance not found. Record will not be displayed.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void StartGame()
     {
+        GameManager.Instance.RestartGame();
         // Load the main game scene
         UnityEngine.SceneManagement.SceneManager.LoadScene("My Game");
     }
